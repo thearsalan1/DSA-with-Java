3cacc4f458902d4fc8dda7fc7f8380bb1a4c134e
thearsalan1 <arsalanloq@gmail.com>
2026-01-28 12:32:26 +0530

    Revising functions

---
+++ Functions/BuiltIn.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.min(3.5,5));
+        System.out.println(Math.cbrt(2345));
+        System.out.println(Math.max(33,9.0));
+    }
+}
---
+++ Functions/Syntax.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class Syntax {
+    public  static void Arslan(){
+        System.out.println("Arsalan");
+    }
+    public static void main(String[] args) {
+        Arslan();
+    }
+}
